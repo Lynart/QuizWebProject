3e3cba9306e6734e2edd8d40cdf3cd5de6259617
Vince Lee <vince.lee.lien@gmail.com>
2016-03-19 17:54:58 -0400

    Login works However, I have no clue how to store the state of a session?

--- src/ca/javaTheHutt/Servlet/ContextManager.java
+++ src/ca/javaTheHutt/Servlet/ContextManager.java
@@ -7,36 +7,60 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import ca.javaTheHutt.model.*;
 
 public class ContextManager {
-	
+
 	private EntityManagerFactory emf;
 	private EntityManager em;
 
-	//Create the EntityManager
-	public ContextManager(){
+	// Create the EntityManager
+	public ContextManager() {
 		emf = Persistence.createEntityManagerFactory("QuizWebProject");
 		em = emf.createEntityManager();
 	}
 
-	//Stub, implement later
-	//Need a user object or user name (make it hacky who cares)
-	public Quiz GenerateQuiz(){
+	// Stub, implement later
+	// Need a user object or user name (make it hacky who cares)
+	public Quiz GenerateQuiz() {
 		Quiz q = new Quiz();
 		return q;
 	}
-	
-	//Method is not tested; need to test!
-	public void AddQuestion(Question q){
+
+	public User Login(String username, String password) {
+		Object user;
+		String query = "FROM "
+						+User.class.getSimpleName()
+						+" e WHERE e.password='"+password+
+						"' AND e.login='"+username+"'";
+		//There has go to to be a less stupid way to do this
+		//Check if user exists
+		try{
+		user = em.createQuery(query).getSingleResult();
+		}
+		//If user does not exist, exception will be thrown
+		catch(NoResultException e){
+			user = null;
+		}
+		//If user is not null, login was successful
+		if(user!=null){
+			return (User) user;
+		}
+		//Login failed
+		return null;
+	}
+
+	// Method is not tested; need to test!
+	// Do we need to manually add answers? Test this
+	public void AddQuestion(Question q) {
 		em.getTransaction().begin();
 		em.persist(q);
-		//Add all answers
-		while(q.getAnswers().iterator().hasNext()){
+		// Add all answers
+		while (q.getAnswers().iterator().hasNext()) {
 			em.persist(q.getAnswers().iterator().next());
 		}
 		em.getTransaction().commit();
 	}
-	
-	//This method is probably not needed?
-	public static DataSource getMySQLDataSource(){
+
+	// This method is probably not needed?
+	public static DataSource getMySQLDataSource() {
 		MysqlDataSource mysqlDS = null;
 		mysqlDS = new MysqlDataSource();
 		mysqlDS.setURL("jdbc:mysql://zenit.senecac.on.ca/dps904_161a11");
@@ -44,11 +68,11 @@ public class ContextManager {
 		mysqlDS.setPassword("jmQL4399");
 		return mysqlDS;
 	}
-	
-	//Destructor; manually call it when you stop using this class!
-	public void Destroy(){
+
+	// Destructor; manually call it when you stop using this class!
+	public void Destroy() {
 		em.close();
 		emf.close();
 	}
-	
+
 }
--- src/ca/javaTheHutt/Servlet/IndexServlet.java
+++ src/ca/javaTheHutt/Servlet/IndexServlet.java
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import ca.javaTheHutt.model.User;
 import ca.javaTheHutt.testers.Register;
 
 /**
@@ -22,27 +23,24 @@ import ca.javaTheHutt.testers.Register;
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	DataSource ds = null;
-	Connection conn = null;
+	ContextManager cm;
+	User user;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public IndexServlet() {
 		super();
-		// TODO Auto-generated constructor stub
+		cm = new ContextManager();
+
 	}
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		/*
-		ds = DataSourceFactory.getMySQLDataSource();
-		try {
-			conn = ds.getConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		*/
+		 * ds = DataSourceFactory.getMySQLDataSource(); try { conn =
+		 * ds.getConnection(); } catch (SQLException e) { e.printStackTrace(); }
+		 */
 	}
 
 	/**
@@ -52,14 +50,31 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
-		
-		PrintWriter out = response.getWriter();
-		Register r = new Register();
-		out.println(r.main(new String[1]));
-		
-		 response.getWriter().append("Served at: Murloc").append(request.getContextPath());
+
+		if (user == null) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+			response.getWriter().println("<p>WOO FUCKING LOO!</p>");
+			rd.include(request, response);
+			//rd.forward(request, response);
+			
+			
+		} else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+
+			PrintWriter out = response.getWriter();
+			Register r = new Register();
+			out.println(r.main(new String[1]));
+			response.getWriter().append("Served at: Murloc").append(request.getContextPath());
+		}
 	}
+	
+	/*
+        	RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+            PrintWriter out= response.getWriter();
+            out.println("<p style=\"color:red;\">Either email or password is wrong. "
+            		+ "Please try again.</p>");
+            rd.include(request, response);
+	 * */
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
@@ -68,17 +83,30 @@ public class IndexServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+        String email = request.getParameter("email");
+        String pwd = request.getParameter("password");
+        
+        User u = cm.Login(email, pwd);
+        if(u==null){
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+			response.getWriter().println("<p>WOO FUCKING LOO!</p>");
+			rd.include(request, response);
+        }
+        else{
+			PrintWriter out = response.getWriter();
+			Register r = new Register();
+			response.getWriter().append("WOOOOOOOOLOOOOOOOOOOOLOOOOOOOOOOO!").append(request.getContextPath());
+        }
 	}
 
 	public void destroy() {
-		if (conn != null) {
+		if (cm != null) {
 			try {
-				conn.close();
-			} catch (SQLException e) {
+				cm.Destroy();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			conn = null;
+			cm = null;
 		}
 	}
 }
